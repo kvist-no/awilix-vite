d3f77056f9250b60a74ba205ed2e2dd643f52f50
Mathias Picker <mattis123@live.no>
2024-07-27 18:45:32 +0200

    added declaration file and updated package json

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+/**
+ * @typedef {Object<string, () => Promise<any>>} DynamicImportModules
+ * @typedef {Object<string, any>} StaticImportModules
+ * @typedef {{ resolverOptions?: import('awilix').BuildResolverOptions, formatName?: (name: string) => string }} LoadOptions
+ */
+/**
+ * @description
+ * @param {import('awilix').AwilixContainer} container The container where the modules should be registered
+ * @param {DynamicImportModules|StaticImportModules} globResult - The result of either doing import.meta.glob('/*.ts') or import.meta.glob('/*.ts', { eager: true })
+ * @param {LoadOptions} options
+ */
+export function loadModules(container: import('awilix').AwilixContainer, globResult: DynamicImportModules | StaticImportModules, options: LoadOptions): Promise<void>;
+export type DynamicImportModules = {
+    [x: string]: () => Promise<any>;
+};
+export type StaticImportModules = {
+    [x: string]: any;
+};
+export type LoadOptions = {
+    resolverOptions?: import("awilix").BuildResolverOptions<any>;
+    formatName?: (name: string) => string;
+};
